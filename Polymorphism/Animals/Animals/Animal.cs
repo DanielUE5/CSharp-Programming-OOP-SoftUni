dbbd40d0765ff0dc573e15471acfa0baceffc2af
Daniel <dani94@mail.bg>
2026-01-11 21:00:50 +0200

    Add files via upload

---
+++ Polymorphism/Animals/Animals/Animal.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    public class Animal
+    {
+        protected Animal(string name, string favouriteFood)
+        {
+            Name = name;
+            FavouriteFood = favouriteFood;
+        }
+
+        public virtual string ExplainSelf()
+        {
+            return $"I am {Name} and my favourite food is {FavouriteFood}";
+        }
+
+        public string Name { get; }
+        public string FavouriteFood { get; }
+    }
+}
---
+++ Polymorphism/Shapes/Shapes/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using Shapes;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        Shape rectangle = new Rectangle(5, 10);
+        Console.WriteLine(rectangle.Draw());
+        Console.WriteLine($"Perimeter: {rectangle.CalculatePerimeter():F2}");
+        Console.WriteLine($"Area: {rectangle.CalculateArea():F2}");
+
+        Shape circle = new Circle(7);
+        Console.WriteLine(circle.Draw());
+        Console.WriteLine($"Perimeter: {circle.CalculatePerimeter():F2}");
+        Console.WriteLine($"Area: {circle.CalculateArea():F2}");
+    }
+}
